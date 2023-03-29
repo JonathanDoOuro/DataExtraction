ac21ef061382b5fe2bace05873e2195a6991e7cc
Jonathan do Ouro <j248364@dac.unicamp.br>
2023-03-29 14:25:57 -0300

    cria as pastas data e source

---
+++ source/extractor.py
@@ -0,0 +1,59 @@
+import PyPDF2 as pd
+import re
+
+### Function to Extract Text From PDF
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path,"rb") as f:
+        reader = pd.PdfReader(f)
+        results = []
+        for i in range(0,len(reader.pages)): # prev read.getNumPages()
+            selected_page = reader.pages[i]
+            text = selected_page.extract_text()
+            results.append(text)
+        return ' '.join(results) # convert list to a single doc
+        
+
+def splitQuestions(text):
+    for i in range(1, 72):
+        ls = [int(x) for x in str(i+1)]
+        li = [int(x) for x in str(i)]
+
+        if (i >= 10):
+            pattern = f'(?s)((?<=QUESTÃO {i})|(?<=QUESTÃO {li[0]} {li[1]})).*?((?=QUESTÃO {i+1})|(?=QUESTÃO {ls[0]} {ls[1]}))'
+        else:
+            pattern = f'(?s)(?<=QUESTÃO {i}).*?(?=QUESTÃO {i+1})'
+
+        rawQuestion = re.search(pattern, text)
+        
+        if (rawQuestion != None):
+            question = rawQuestion.group(0)
+            with open(f'data/output/question{i}.txt', 'w') as file:
+                print(question, file=file)
+
+if __name__ == '__main__':
+    path = "./data/input/f12022Q_X.pdf"
+    #extrair o texto completo e salvar ele
+    text = extract_text_from_pdf(path)
+    with open(f'data/output/completeText.txt', 'w') as file:
+        print(text, file=file)
+
+    #separa as questões e salva em arquivos diferentes
+    splitQuestions(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
