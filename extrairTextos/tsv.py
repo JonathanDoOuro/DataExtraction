f95dde36d3feb1b17624809c5b8bbec4af48587f
Jonathan do Ouro <j248364@dac.unicamp.br>
2023-06-15 12:23:41 -0300

    refatora o processamento dos textos

--- extrairTextos/tsv.py
+++ extrairTextos/tsv.py
@@ -3,20 +3,11 @@ import json
 import random
 import nltk
 import re
-from nltk.stem import WordNetLemmatizer
 import string
 import spacy
+import time
 
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
-
-with open("extrairTextos/DataSetGeral/biologia_train_dataset.json", "r") as file:
-    docs = json.load(file)
-
-textos, titulos, keywords = zip(*docs)
-
-def preprocess_text(textos):
+def preprocess_text0(textos):
     # Carrega o modelo do SpaCy para o idioma português
     nlp = spacy.load("pt_core_news_sm")
     
@@ -39,73 +30,114 @@ def preprocess_text(textos):
     
     return textos_processados
 
-porcentagem_train = 0.8
-porcentagem_test = 0.1
-porcentagem_val = 0.1
+def preprocess_text1(textos):
+    # Carrega o modelo do SpaCy para o idioma português
+    nlp = spacy.load("pt_core_news_md")
+    
+    # Cria um conjunto de pontuações para verificação eficiente
+    pontuacao = set(string.punctuation)
+    
+    # Lista para armazenar os textos processados
+    textos_processados = []
+    
+    # Processa cada texto
+    for texto in textos:
+        # Converte o texto para minúsculas
+        texto = texto.lower()
+        
+        # Remove pontuações e números
+        texto_processado = ''.join(char for char in texto if char not in pontuacao and not char.isdigit())
+        
+        # Aplica o pipeline do SpaCy para lematização e remoção de stop words
+        doc = nlp(texto_processado)
+        
+        # # Lematização das palavras e remoção de stop words
+        palavras_lematizadas = [token.lemma_ for token in doc if not token.is_stop]
+        
+        #Reconstroi o texto a partir das palavras lematizadas
+        texto_processado = ' '.join(palavras_lematizadas)
+        
+        # Adiciona o texto processado à lista de textos processados
+        textos_processados.append(texto_processado)
+    
+    return textos_processados
 
-# Dados para as colunas
-coluna1 = preprocess_text(textos)
-# Criar a lista coluna2
-total_elementos = len(coluna1)
-num_train = int(total_elementos * porcentagem_train)
-num_test = int(total_elementos * porcentagem_test)
-num_val = total_elementos - num_train - num_test
-coluna2 = ["train"] * num_train + ["test"] * num_test + ["val"] * num_val
-random.shuffle(coluna2)
+def preprocess_text(textos, nlp_model):
+    inicio = time.time()
+    print(inicio)
+    pontuacao = set(string.punctuation)
+    textos_processados = [
+        ' '.join(token.lemma_ for token in nlp_model(texto.lower()) if not token.is_stop and not token.is_punct and not token.is_digit and not any(char in pontuacao for char in str(token))) for texto in textos
+    ]
+    fim = time.time()
+    tempo_execucao = fim - inicio
+    print("Tempo de execução:", tempo_execucao, "segundos")
+    return textos_processados
 
-coluna3 = titulos
+def particao(n: int):
+    porcentagem_train = 0.8
+    porcentagem_test = 0.1
+    num_train = int(n * porcentagem_train)
+    num_test = int(n * porcentagem_test)
+    num_val = n - num_train - num_test
+    coluna2 = ["train"] * num_train + ["test"] * num_test + ["val"] * num_val
+    random.shuffle(coluna2)
+    return coluna2
 
-# Caminho para o arquivo de saída
-caminho_arquivo = 'extrairTextos/DataSetOctis/corpus.tsv'
+def salvarCorpus(caminho_arquivo, coluna1, coluna2, coluna3):
+    with open(caminho_arquivo, 'w', newline='', encoding='utf-8') as arquivo_tsv:
+        writer = csv.writer(arquivo_tsv, delimiter='\t')
 
-# Abre o arquivo em modo de escrita
-with open(caminho_arquivo, 'w', newline='', encoding='utf-8') as arquivo_tsv:
-    writer = csv.writer(arquivo_tsv, delimiter='\t')
+        # Escreve os dados em cada linha
+        for i in range(len(coluna1)):
+            writer.writerow([coluna1[i], coluna2[i], coluna3[i]])
 
-    # Escreve os dados em cada linha
-    for i in range(len(coluna1)):
-        writer.writerow([coluna1[i], coluna2[i], coluna3[i]])
+def lerDataSet():
+    with open("extrairTextos/DataSetGeral/biologia_test_dataset.json", "r") as file:
+        docs = json.load(file)
 
-# Baixa a lista de stop words (se ainda não foi baixada)
+    textos, titulos, keywords = zip(*docs)
 
+    return (textos, titulos, keywords)
 
-stopwords_personalizadas = ["e/ou"]
+def vocabulario(textos: list):
+    stopwords_personalizadas = ["e/ou"]
+    stopwords = nltk.corpus.stopwords.words('portuguese')
+    stopwords.extend(stopwords_personalizadas)
+    # Expressão regular para filtrar apenas palavras
+    regex = re.compile(r'\b[a-zA-Z]+\b')
 
-# Carrega a lista de stop words em português
-stopwords = nltk.corpus.stopwords.words('portuguese')
-stopwords.extend(stopwords_personalizadas)
+    vocabulario = []
 
+    for texto in textos:
+        # Tokeniza o texto em palavras
+        palavras = nltk.word_tokenize(texto.lower())
 
-# Expressão regular para filtrar apenas palavras
-regex = re.compile(r'\b[a-zA-Z]+\b')
+        # Filtra as palavras que não são stop words nem números
+        palavras_filtradas = [palavra for palavra in palavras if palavra not in stopwords and not palavra.isdigit() and regex.match(palavra)]
 
-# Lista para armazenar as palavras do vocabulário
-vocabulario = []
-lemmatizer = WordNetLemmatizer()
-# Itera sobre os textos
-for texto in textos:
-    # Tokeniza o texto em palavras
-    palavras = nltk.word_tokenize(texto.lower())
+        vocabulario.extend(palavras_filtradas)
 
-    # Filtra as palavras que não são stop words nem números
-    palavras_filtradas = [palavra for palavra in palavras if palavra not in stopwords and not palavra.isdigit()]
+    return list(set(vocabulario))
 
-    # Aplica a expressão regular para filtrar apenas palavras
-    palavras_filtradas = [palavra for palavra in palavras_filtradas if regex.match(palavra)]
+def salvarVocabulario(caminho_arquivo, vocabulario: list):
+    with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+        for palavra in vocabulario:
+            arquivo.write(palavra + '\n')
 
-    palavras_lematizadas = [lemmatizer.lemmatize(palavra) for palavra in palavras_filtradas]
+def main():
+    nlp = spacy.load("pt_core_news_md")
+    caminho_corpus = 'extrairTextos/DataSetOctis/corpus.tsv'
+    dataSet = lerDataSet()
+    coluna1 = preprocess_text(dataSet[0], nlp)
+    coluna2: list = particao(len(dataSet[0]))
+    coluna3 = dataSet[1]
+    
+    salvarCorpus(caminho_corpus, coluna1, coluna2, coluna3)
 
-    # Adiciona as palavras filtradas ao vocabulário
-    vocabulario.extend(palavras_filtradas)
+    caminho_vocabulario = 'extrairTextos/DataSetOctis/vocabulary.txt'
+    vocabulario1 = vocabulario(coluna1)
+    salvarVocabulario(caminho_vocabulario, vocabulario1)
 
-# Remove duplicatas do vocabulário
-vocabulario = list(set(vocabulario))
-
-# Caminho para o arquivo de saída
-caminho_arquivo = 'extrairTextos/DataSetOctis/vocabulary.txt'
-
-# Abre o arquivo em modo de escrita
-with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
-    # Escreve uma palavra por linha
-    for palavra in vocabulario:
-        arquivo.write(palavra + '\n')
+if __name__ == "__main__":
+    main()
